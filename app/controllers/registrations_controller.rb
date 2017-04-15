30df17fa1268d8dc8e4364ec7b61d4b8b97102a0
Izabela Lima <izamglima@gmail.com>
2017-04-14 22:46:10 -0300

    adicionado campo nome no cadastro

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,7 +1,7 @@
 class RegistrationsController < Devise::RegistrationsController
   protected
 
-  def after_sign_up_path_for(user)
-    edit_user_registration_path(user)
+  def sign_up_params  
+	params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
 end	
