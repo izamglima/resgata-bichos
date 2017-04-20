fb1358d1ac63aa76a60d98e6a54e8c74d325a536
Izabela Lima <izamglima@gmail.com>
2017-04-20 20:16:49 -0300

    adicionado bucket para armazenar as imagens

--- app/models/image.rb
+++ app/models/image.rb
@@ -1,6 +1,14 @@
 class Image < ApplicationRecord
 	belongs_to :animal
-	has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
-	validates_attachment :avatar,
-  content_type: { content_type: ["image/jpeg", "image/gif", "image/png"] }
+	has_attached_file :image,
+					  :storage => :s3,
+					  :s3_region => "us-west-1",
+					  :bucket => "resgataimgs",
+					  :s3_host_name => "s3-us-west-1.amazonaws.com", 
+					  :s3_credentials => {:access_key_id  => "AKIAI43CK576YD4FOM2Q",
+											:secret_access_key => "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"
+										}
+	validates_attachment :image,
+  		content_type: { content_type: ["image/jpeg", "image/gif", "image/png"] }
 end
+
