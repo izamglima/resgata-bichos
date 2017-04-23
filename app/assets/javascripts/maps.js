bda4fd911ea24e4bb4572e940fcf86906f94d85d
Izabela Lima <izamglima@gmail.com>
2017-04-23 20:15:28 -0300

    agora sim maps funfando

--- app/assets/javascripts/maps.js
+++ app/assets/javascripts/maps.js
@@ -1,13 +1,28 @@
-(function (window, google) {
-	//map options
-	var options = {
-		center: {
-			lat: -34.397,
-			lng: 150.644
-		},
-		zoom: 10
-	},
-	element = document.getElementById('map-canvas'),
-	//map
-	map = new google.maps.Map(element, options);
-}(window, window.google));
+var Maps = (function (module) {
+	module.init = function() {
+		var element = document.getElementById('map-canvas');
+		if (!element) {
+			return false;
+		}
+
+		//map options
+		var options = {
+			center: {
+				lat: -34.397,
+				lng: 150.644
+			},
+			zoom: 10
+		};
+
+		var map = new google.maps.Map(element, options);		
+		}		
+	};
+
+	return module;
+}(Maps || {}));
+
+$(document).on('turbolinks:load', function() {
+	if (window.google && window.google.maps) {
+		Maps.init();
+	};
+});
