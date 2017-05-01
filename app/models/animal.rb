6967d22961ed6020fe695139ec9f290a63fd9a2d
Izabela Lima <izamglima@gmail.com>
2017-04-30 22:16:05 -0300

    alterações visuais

--- app/models/animal.rb
+++ app/models/animal.rb
@@ -8,5 +8,5 @@ class Animal < ApplicationRecord
 	validates :animal_type, presence: true
 	belongs_to :user
 	has_many :images, dependent: :destroy
-	has_many :events
+	has_many :events, dependent: :destroy
 end
