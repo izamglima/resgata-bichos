c6ee63a31bdd9027b92ce4eefe78844a3c745ea6
Izabela Lima <izamglima@gmail.com>
2017-05-09 19:15:18 -0300

    query

--- app/controllers/searchs_controller.rb
+++ app/controllers/searchs_controller.rb
@@ -1,8 +1,8 @@
 class SearchsController < ApplicationController
 	def search
 		@animals = Animal.all
-		if params[:animal_type]
-		    @animals = Animal.search(params[:animal_type]).order("created_at DESC")
+		if params[:search]
+		    @animals = Animal.search(params[:search]).order("created_at DESC")
 		else
 		    @animals = Animal.all.order('created_at DESC')
 		end
