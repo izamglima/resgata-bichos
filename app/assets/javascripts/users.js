8e54f511580c3a431059af08c92db63cf54741df
Izabela Lima <izamglima@gmail.com>
2017-06-06 08:57:51 -0300

    melhorando front mobile

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -2,53 +2,40 @@ function validateform(){
 	var inputEmail = $('#user_email');
 	var inputSenha = $('#user_password');
 	var errors = [];
+	
 	if ($(inputEmail).val() == "") {
-		$(inputEmail).parent().tooltip({
-    				trigger: 'manual',
-                    title: 'Por favor, preencha o campo.',
-                    template: '<div class="tooltip" role="tooltip"><div class="tooltip-arrow fail"></div><div class="tooltip-inner fail"></div></div>',
-                    placement: 'top'
-    			}).tooltip('show');
+		callTollTip(inputEmail);
+
 		errors.push(1);
 		$(inputEmail).on('keyup', function() {
 			$(inputEmail).parent().tooltip('hide');
 		})
 	}
+
 	if ($(inputSenha).val() == "") {
-		$(inputSenha).parent().tooltip({
-    				trigger: 'manual',
-                    title: 'Por favor, preencha o campo.',
-                    template: '<div class="tooltip" role="tooltip"><div class="tooltip-arrow fail"></div><div class="tooltip-inner fail"></div></div>',
-                    placement: 'top'
-    			}).tooltip('show');
+		callTollTip(inputSenha);
+
 		errors.push(1);
 		$(inputSenha).on('keyup', function() {
 			$(inputSenha).parent().tooltip('hide');
 		})
 	}
+
 	if($('.cadastro').length) {
 		var inputNome = $('#user_name');
 		var inputPass = $('#user_password_confirmation');
 		
 		if ($(inputPass).val() == "") {
-			$(inputPass).parent().tooltip({
-	    				trigger: 'manual',
-	                    title: 'Por favor, preencha o campo.',
-	                    template: '<div class="tooltip" role="tooltip"><div class="tooltip-arrow fail"></div><div class="tooltip-inner fail"></div></div>',
-	                    placement: 'top'
-	    			}).tooltip('show');
+			callTollTip(inputPass);
+
 			errors.push(1);
 			$(inputPass).on('keyup', function() {
 				$(inputPass).parent().tooltip('hide');
 			})
 		}
 		if ($(inputNome).val() == "") {
-			$(inputNome).parent().tooltip({
-	    				trigger: 'manual',
-	                    title: 'Por favor, preencha o campo.',
-	                    template: '<div class="tooltip" role="tooltip"><div class="tooltip-arrow fail"></div><div class="tooltip-inner fail"></div></div>',
-	                    placement: 'top'
-	    			}).tooltip('show');
+			callTollTip(inputNome);
+			
 			errors.push(1);
 			$(inputNome).on('keyup', function() {
 				$(inputNome).parent().tooltip('hide');
