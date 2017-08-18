dd984fcb1897756c2191a4c1ed64c2343cfd8291
Izabela Lima <izamglima@gmail.com>
2017-08-18 16:44:49 -0300

    configs

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,9 +15,3 @@
 //= require bootstrap.min
 //= require jquery.mask.min
 //= require_tree .
-if (navigator.serviceWorker) {
-  navigator.serviceWorker.register('/serviceworker.js', { scope: './' })
-    .then(function(reg) {
-      console.log('[Page] Service worker registered!');
-    });
-}
--- config/initializers/serviceworker.rb
+++
@@ -1,6 +0,0 @@
-Rails.application.configure do
-  config.serviceworker.routes.draw do
-    match "/serviceworker.js"
-    match "/manifest.json"
-  end
-end
