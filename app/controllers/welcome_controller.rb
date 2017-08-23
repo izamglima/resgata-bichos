90e6d4980b5c9f90a2615ad7d227ab8d3069e1a5
Izabela Lima <izamglima@gmail.com>
2017-08-23 12:30:56 -0300

    adicionado certificado ssl 2

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,7 +2,7 @@ class WelcomeController < ApplicationController
 	#before_action :authenticate_user!
 	def letsencrypt
       
-      render text: "wI173GWLCd7OnHfsJXLIc2oethZWWNDIV3d8xxg47lQ.JvHtQFGrilvU7fr_NprfeU6kxlG9G6UUS4xEy771ygI"
+      render text: "rDPmUvyAY8_fPj1TZ8_RIbbEU8pJUzyPU55L5KQwLw4.JvHtQFGrilvU7fr_NprfeU6kxlG9G6UUS4xEy771ygI"
       
     end
 end
