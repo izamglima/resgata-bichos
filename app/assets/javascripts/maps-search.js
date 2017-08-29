fce9dfec426c0047f076d66231e499b947341c1e
Izabela Lima <izamglima@gmail.com>
2017-08-29 12:16:54 -0300

    add footer, comentado push do pwa, alterados comentarios no search

--- app/assets/javascripts/maps-search.js
+++ app/assets/javascripts/maps-search.js
@@ -1,90 +1,79 @@
 var eventoData;
-//Esta promise espera a api retornar os dados para então alimentar o array da variável eventoData
-axios('/api/search').then(function(response) { 
+if ($('.search').length > 0) {
+	//Esta promise espera a api retornar os dados para então alimentar o array da variável eventoData
+	axios('/api/search').then(function(response) { 
 
-  eventoData = response.data.map(function(evento) {
-    return {latitude: evento.latitude, longitude: evento.longitude, status: evento.status, id: evento.id};
-  });
-  //chamada do mapa
-  initialize();
+	  eventoData = response.data.map(function(evento) {
+	    return {latitude: evento.latitude, longitude: evento.longitude, status: evento.status, id: evento.id};
+	  });
+	  //chamada do mapa
+	  initialize();
 
-});
-// Esta função vai percorrer a informação contida na variável eventoData
-// e cria os marcadores através da função createMarker
-function displayMarkers(){
+	});    
+};
 
-   // esta variável vai definir a área de mapa a abranger e o nível do zoom
-   // de acordo com as posições dos marcadores
+function displayPins(){
+   // define a área de mapa a exibir
    var bounds = new google.maps.LatLngBounds();
 
-   // Loop que vai percorrer a informação contida em eventoData 
-   // para que a função createMarker possa criar os marcadores 
-   for (var i = 0; i < eventoData.length; i++){
+    //percorre o eventoData e cria marcadores com o conteúdo
+	for (var i = 0; i < eventoData.length; i++){
+		var latlng = new google.maps.LatLng(eventoData[i].latitude, eventoData[i].longitude);
+		var nome = eventoData[i].nome;
+		var status = eventoData[i].status;
+		var id = eventoData[i].id;
 
-      var latlng = new google.maps.LatLng(eventoData[i].latitude, eventoData[i].longitude);
-      var nome = eventoData[i].nome;
-      var status = eventoData[i].status;
-      var id = eventoData[i].id;
+		createPins(latlng, nome, status, id);
 
-      createMarker(latlng, nome, status, id);
+		// Os valores de latitude e longitude do marcador são adicionados à
+		// variável bounds
+		bounds.extend(latlng); 
+	}
 
-      // Os valores de latitude e longitude do marcador são adicionados à
-      // variável bounds
-      bounds.extend(latlng); 
-   }
-
-   // Depois de criados todos os marcadores,
-   // a API, através da sua função fitBounds, vai redefinir o nível do zoom
-   // e consequentemente a área do mapa abrangida de acordo com
-   // as posições dos marcadores
+   // Aqui a API redefine o nível do zoom de acordo com as posições passadas dos casos registrados
    map.fitBounds(bounds);
 }
 
-// Função que cria os marcadores e define o conteúdo de cada Info Window.
-function createMarker(latlng, nome, status, id){
-   var marker = new google.maps.Marker({
-      map: map,
-      position: latlng,
-      title: nome
-   });
+// cria e define o conteúdo dos marcadores
+function createPins(latlng, nome, status, id){
+	var marker = new google.maps.Marker({
+		map: map,
+		position: latlng,
+		title: nome
+	});
 
-   // Evento que dá instrução à API para estar alerta ao click no marcador.
-   // Define o conteúdo e abre a Info Window.
-   google.maps.event.addListener(marker, 'click', function() {
+   	// Adiciona um listener para o evento click para montar o conteúdo da infowindow
+   	google.maps.event.addListener(marker, 'click', function() {
       
-      // Variável que define a estrutura do HTML a inserir na Info Window.
-      var iwContent = '<div id="iw_container">' +
-      '<div class="iw_title">' + nome + '</div>' +
-      '<div class="iw_title">' + id + '</div>' +
-      '<div class="iw_content">' + status + '</div></div>';
-      
-      // O conteúdo da variável iwContent é inserido na Info Window.
-      infoWindow.setContent(iwContent);
+		// Variável que define a estrutura do HTML a inserir na Info Window.
+		var infoContent = '<div id="container-infos">' +
+		'<div class="title-info">' + nome + '</div>' +
+		'<div class="title-info">' + id + '</div>' +
+		'<div class="content-info">' + status + '</div></div>';
 
-      // A Info Window é aberta com um click no marcador.
-      infoWindow.open(map, marker);
+		// seta o conteúdo da infoContent
+		infos.setContent(infoContent);
+
+		// Este é o evento que abre o info window
+		infos.open(map, marker);
    });
 }
 
 function initialize() {
-   var mapOptions = {
-      center: new google.maps.LatLng(-22.8533142,-47.1294165),
-      zoom: 9,
-      mapTypeId: 'roadmap',
-   };
+	var mapOptions = {
+		center: new google.maps.LatLng(-22.8533142,-47.1294165),
+		zoom: 12,
+		mapTypeId: 'roadmap',
+	};
+	map = new google.maps.Map(document.getElementById('map-search'), mapOptions);
 
-  map = new google.maps.Map(document.getElementById('map-search'), mapOptions);
+	// Cria apenas, o conteudo é adicionado no createPins
+	infos = new google.maps.InfoWindow();
 
-  // Cria a nova Info Window com referência à variável infoWindow.
-  // O conteúdo da Info Window é criado na função createMarker.
-  infoWindow = new google.maps.InfoWindow();
+	google.maps.event.addListener(map, 'click', function() {
+		infos.close();
+	});
 
-  // Evento que fecha a infoWindow com click no mapa.
-  google.maps.event.addListener(map, 'click', function() {
-    infoWindow.close();
-  });
-
-  // Chamada para a função que vai percorrer a informação
-  // contida na variável markersData e criar os marcadores a mostrar no mapa
-  displayMarkers();
+	// Chamada para a função que vai percorrer as informações do eventoData e criar os pins no mapa
+	displayPins();
 }
--- app/assets/javascripts/serviceworker-companion.js
+++ app/assets/javascripts/serviceworker-companion.js
@@ -1,32 +1,33 @@
-
+//Register the ServiceWorker
 if (navigator.serviceWorker) {
-
-	navigator.serviceWorker.register('/serviceworker.js', { scope: './' })
-    .then(function(reg) {
-		console.log('Service Worker registration sucessful!');
-  	})
+	if (navigator.serviceWorker.controller) {
+		console.log('[PWA Builder] active service worker found, no need to register')
+	} else {
+		navigator.serviceWorker.register('/serviceworker.js', { scope: './' })
+    	.then(function(reg) {
+			console.log('Service Worker registration sucessful!');
+  		})
+	}
 }
 
 Notification.requestPermission(function(status) {
   console.log('Notification permission status:', status);
 });
 
-if (Notification.permission == 'granted') {
-  navigator.serviceWorker.getRegistration().then(function(reg) {
+// if (Notification.permission == 'granted') {
+//   navigator.serviceWorker.getRegistration().then(function(reg) {
 
-    var options = {
-	  body: 'First notification!',
-	  //icon: 'images/notification-flat.png',
-	  vibrate: [100, 50, 100],
-	  data: {
-	    dateOfArrival: Date.now(),
-	    primaryKey: 1
-	  },
+//     var options = {
+// 	  body: 'Obrigada por se registrar no Resgata Bichos!',
+// 	  //icon: 'images/notification-flat.png',
+// 	  vibrate: [100, 50, 100],
+// 	  data: {
+// 	    dateOfArrival: Date.now(),
+// 	    primaryKey: 1
+// 	  },
 
-	  // TODO 5.1 - add a tag to the notification
+// 	};
 
-	};
-
-    reg.showNotification('Hello world!', options);
-  });
-}
+//     reg.showNotification('Olá, bem vindo!', options);
+//   });
+// }
